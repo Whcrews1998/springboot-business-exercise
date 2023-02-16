a9c8460c5879bffda467819d05bb8ab04b63a9a6
Harris Crews <whcrews1998@gmail.com>
2023-02-16 08:27:31 -0500

    Initial setup of project

---
+++ src/main/java/com/whc1998/businessexercise/BusinessExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.whc1998.businessexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/whc1998/businessexercise/BusinessExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whc1998.businessexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
