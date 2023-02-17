c7b136a62ec281d6eff39ee2eecb6df087d5ac2a
Harris Crews <whcrews1998@gmail.com>
2023-02-17 08:09:42 -0500

    Added data insertion into H2 database.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into BUSINESS(id, name) values(1, 'Harris Inc');
+
+insert into EQUIPMENT(id, name, business_id) values(10,'computer', 1);
+insert into employee(id, position, first_name, last_name, salary, business_id) values(100, 'Software Developer', 'Harris', 'Crews', 100, 1);
